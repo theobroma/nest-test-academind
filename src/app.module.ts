0b60b737377fcba04db93b102c99ea7598513e28
Alexandr Siryj <theobroma222@gmail.com>
2022-03-26 02:59:08 +0200

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { ProductsModule } from './products/products.module';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://theobroma:<password>@cluster0.ax7qh.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      // 'mongodb+srv://maximilian:B3dqPzooRLzFiVYm@cluster0-ntrwp.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
